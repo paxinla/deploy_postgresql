2c09e2db1e1e12305be964872df8be8a27c61d38
paxinla <paxinla@gmail.com>
2021-04-20 14:33:44 +0800

    fix 修改安装脚本中的拼写错误。

--- conf/pg_start.sh
+++ conf/pg_start.sh
@@ -2,14 +2,16 @@
 # This script should be owned by root.
 
 
+echo "= PostgreSQL"
 if ! pgrep -f '/data/pgdata' > /dev/null
 then
-    echo -1000 > /proc/self/oom_score_adj
+    echo -1000 | sudo tee /proc/self/oom_score_adj
     export PG_OOM_ADJUST_FILE=/proc/self/oom_score_adj
     export PG_OOM_ADJUST_VALUE=0
-    sudo -u postgres /opt/postgresql/bin/pg_ctl start -D /data/pgdata
+    sudo -u postgres /opt/postgresql/bin/pg_ctl start -l /tmp/start_pg.log -D /data/pgdata
 fi
 
+echo "= PgBouncer"
 if ! pgrep -f 'pgbouncer.ini' > /dev/null
 then
     sudo -u postgres /opt/pgbouncer/bin/pgbouncer -d /dbms/conf/postgresql/pgbouncer.ini
@@ -17,7 +19,11 @@ fi
 
 sleep 6m
 
+test -d /var/run/postgresql || { sudo mkdir -p /var/run/postgresql && sudo chown -R postgres:postgres /var/run/postgresql ; }
+
+echo "= RepMgr"
 if ! pgrep -f 'repmgr.conf' > /dev/null
 then
-    sudo -u postgres /opt/postgresql/bin/repmgrd -f /dbms/conf/postgresql/repmgr.conf --monitoring-history --daemonize --pid-file=/var/run/postgresql/repmgrd.pid | sudo tee -a /var/log/pg_log/repmgr.log 2>&1
+    sudo -u postgres /opt/postgresql/bin/repmgrd -f /dbms/conf/postgresql/repmgr.conf --monitoring-history --daemonize --pid-file=/var/run/postgresql/repmgrd.pid | sudo -u postgres tee -a /var/log/pg_log/repmgr.log 2>&1
 fi
+
